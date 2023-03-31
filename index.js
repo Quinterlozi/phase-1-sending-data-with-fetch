cddd4f81ce82910b42655ec99e5a7b8c7fad4ed2
Quinterlozi <jojobobo254@gmail.com>
2023-03-31 10:38:16 +0300

    Assignment Done

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+function submitData(name, email) {
+    let formData = {
+      name: name,
+      email: email
+    };
+  
+    let configObj = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(formData)
+    };
+  
+    return fetch("http://localhost:3000/users", configObj)
+    .then(response => response.json())
+    .then(data => {
+      document.body.innerHTML += data.id;
+    })
+    .catch(error => {
+      document.body.innerHTML += error.message;
+    });
+}
+    
+
+
